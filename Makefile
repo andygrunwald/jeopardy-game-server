2a1add02c2eb3e42f5dc1cd6e9f4d00c84410ee7
Andy Grunwald <andygrunwald@gmail.com>
2019-03-16 23:09:31 +0100

    Buzzer support hacked in

--- Makefile
+++ Makefile
@@ -2,6 +2,12 @@
 install:
 	go build
 
+deploy-pi:
+	GOARM=7 GOARCH=arm GOOS=linux go build && scp -r * pi@192.168.4.1:jeopardy-game-server/
+
+deploy-pi-lan:
+	GOARM=7 GOARCH=arm GOOS=linux go build && scp -r * pi@192.168.178.41:jeopardy-game-server/
+
 .PHONY: run
 run: install
 	./jeopardy-game-server
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
 )
 
 // cache represents the caching instance to avoid I/O calls
@@ -38,6 +43,35 @@ var (
 	date    = "unknown"
 )
 
+// webServer represents the data structure that
+// keeps everything together for the webServer
+// incl. the web sockets
+type webServer struct {
+	Listen         string
+	ButtonHits     chan buttonHit
+	SocketUpgrader websocket.Upgrader
+	SocketClients  map[*websocket.Conn]bool
+}
+
+// buttonHit represents the message that will be sent
+// once a button/buzzer was hit
+type buttonHit struct {
+	// Color is the color of the buzzer that was hit
+	// see constants button* above
+	Color string
+}
+
+const (
+	// buttonRed represents a physical buzzer in color red
+	buttonRed string = "red"
+	// buttonRed represents a physical buzzer in color green
+	buttonGreen string = "green"
+	// buttonRed represents a physical buzzer in color blue
+	buttonBlue string = "blue"
+	// buttonRed represents a physical buzzer in color yellow
+	buttonYellow string = "yellow"
+)
+
 func main() {
 	addr := ":8000"
 	if v := os.Getenv("JGAME_SRV_ADDR"); len(v) > 0 {
@@ -47,6 +81,90 @@ func main() {
 	log.Printf("Jeopardy Game Server ... %v, commit %v, built at %v\n", version, commit, date)
 	log.Println("Jeopardy Game Server ... starting ...")
 
+	//
+	// Start web socket and webserver
+	//
+	buttonHits := make(chan buttonHit, 4)
+	httpServer := &webServer{
+		Listen:     addr,
+		ButtonHits: buttonHits,
+		SocketUpgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// This is not good idea. Why?
+				// See https://github.com/gorilla/websocket/issues/367
+				// But we (assume to) run locally on a
+				// RaspberryPi. And we want to make it work for now ;)
+				return true
+			},
+		},
+		SocketClients: make(map[*websocket.Conn]bool),
+	}
+
+	go httpServer.startWebserver(addr)
+	go httpServer.socketBroadcast()
+
+	// Usage of https://gobot.io/for dealing with
+	// physical buzzers.
+	//
+	// Whatever you do with the GPIO pins
+	// the raw BCM2835 pinout mapping to Raspberry Pi at
+	// https://godoc.org/github.com/stianeikeland/go-rpio
+	// is super helpful.
+	r := raspi.NewAdaptor()
+	red := gpio.NewButtonDriver(r, "40")
+	green := gpio.NewButtonDriver(r, "38")
+	blue := gpio.NewButtonDriver(r, "36")
+	//yellow := gpio.NewButtonDriver(r, "32")
+
+	work := func() {
+		red.On(gpio.ButtonPush, func(data interface{}) {
+			log.Println("Button red pressed")
+			msg := buttonHit{
+				Color: buttonRed,
+			}
+			buttonHits <- msg
+		})
+
+		green.On(gpio.ButtonPush, func(data interface{}) {
+			log.Println("Button green pressed")
+			msg := buttonHit{
+				Color: buttonGreen,
+			}
+			buttonHits <- msg
+		})
+
+		blue.On(gpio.ButtonPush, func(data interface{}) {
+			log.Println("Button blue pressed")
+			msg := buttonHit{
+				Color: buttonBlue,
+			}
+			buttonHits <- msg
+		})
+
+		/*
+			yellow.On(gpio.ButtonPush, func(data interface{}) {
+				log.Println("Button yellow pressed")
+				msg := buttonHit{
+					Color: buttonYellow,
+				}
+				buttonHits <- msg
+			})
+		*/
+	}
+
+	robot := gobot.NewRobot("buttonBot",
+		[]gobot.Connection{r},
+		//[]gobot.Device{red, green, blue, yellow},
+		[]gobot.Device{red, green, blue},
+		work,
+	)
+
+	robot.Start()
+}
+
+// startWebserver will start the webserver incl.
+// websocket - Nothing more.
+func (httpServer *webServer) startWebserver(addr string) {
 	// Determine the path to load the games from
 	gamesPath := "games"
 	if v := os.Getenv("JEOPARDY_GAMES"); len(v) > 0 {
@@ -83,6 +201,9 @@ func main() {
 	r.HandleFunc("/season/{seasonID}", s.SeasonHandler).Methods("GET")
 	r.HandleFunc("/game/{gameID}", s.SeasonGameHandler).Methods("GET")
 
+	log.Printf("Starting websocket on %s ...\n", httpServer.Listen)
+	r.HandleFunc("/socket", httpServer.websocketHandler)
+
 	// Static file server
 	fs := http.FileServer(http.Dir("games"))
 	r.PathPrefix("/").Handler(fs)
@@ -92,5 +213,39 @@ func main() {
 	log.Printf("Jeopardy Game Server ... starting on %s... Done\n", addr)
 	if err := http.ListenAndServe(addr, handlers.LoggingHandler(os.Stdout, r)); err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+// websocketHandler registers new websocket clients
+func (httpServer *webServer) websocketHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := httpServer.SocketUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+
+	// Register new client
+	httpServer.SocketClients[c] = true
+}
+
+// socketBroadcast will send a single message
+// to all connected websocket clients (broadcasting).
+func (httpServer *webServer) socketBroadcast() {
+	log.Println("Starting socket broadcast ...")
+	for {
+		msg := <-httpServer.ButtonHits
+
+		jsonMessage, _ := json.Marshal(msg)
+		log.Printf("Broadcasting message: %v\n", string(jsonMessage))
+
+		// Send to every client that is currently connected
+		for client := range httpServer.SocketClients {
+			err := client.WriteMessage(websocket.TextMessage, jsonMessage)
+			if err != nil {
+				log.Printf("Websocket error: %s", err)
+				client.Close()
+				delete(httpServer.SocketClients, client)
+			}
+		}
 	}
 }
